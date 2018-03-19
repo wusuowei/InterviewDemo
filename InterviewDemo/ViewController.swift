c4f81637efd23ff2ac42eafd97ba2c06ebaa25d9
wentianen <1206860151@qq.com>
2018-03-19 23:13:39 +0800

    Initial Commit

---
+++ InterviewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterviewDemo
+//
+//  Created by wentianen on 2018/3/19.
+//  Copyright © 2018年 wentianen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
