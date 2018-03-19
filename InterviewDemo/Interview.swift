775e041c68a9ed746d5aea138e71e850836e0826
wentianen <1206860151@qq.com>
2018-03-20 00:47:52 +0800

    format

--- InterviewDemo/Interview.swift
+++ InterviewDemo/Interview.swift
@@ -10,8 +10,12 @@ import Foundation
 import UIKit
 
 // 求一个整数数组中和最大的连续子数组，例如：[1, 2, -4, 4, 10, -3, 4, -5, 1]的最大连续子数组是[4, 10, -3, 4]（需写明思路，并编程实现）。
+/*
+ *  思路：
+ *  1、和最大的子序列的第一个元素肯定是正数
+ *  2、因为元素有正有负，因此子序列的最大和一定大于0
+ */
 struct MaxContinuousSubArray {
-    var array = [1, 2, -4, 4, 10, -3, 4, -5, 1]
     func maxContinuousSubArray(_ array: [Int]) -> [Int] {
         guard !array.isEmpty else { return [Int]() }
         guard !(array.count == 1) else { return array }
@@ -34,8 +38,12 @@ struct MaxContinuousSubArray {
         let sub = array.enumerated().filter( { ($0.offset >= startIndex && $0.offset <= endIndex) } )
         return sub.map( { $0.element } )
     }
+    func test() {
+        let array = [1, 2, -4, 4, 10, -3, 4, -5, 1]
+        let sub = maxContinuousSubArray(array)
+        print("\(sub)")
+    }
 }
-
 
 // 图片缓存
 struct ImageCacheError: Error {
